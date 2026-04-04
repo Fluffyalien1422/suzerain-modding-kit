d89471c581c9d40ba24972ec702cf9d444606c6f
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-04-03 18:42:19 -0700

    feat: token indicators for custom story fragments

--- SuzerainModdingKit/GameState.cs
+++ SuzerainModdingKit/GameState.cs
@@ -1,4 +1,5 @@
 using Il2Cpp;
+using MelonLoader;
 using SuzerainModdingKit.StoryFragments;
 
 namespace SuzerainModdingKit;
@@ -6,45 +7,102 @@ namespace SuzerainModdingKit;
 public static class GameState
 {
     public static int? CurrentStepNum => Managers.Instance?.GameFlowManager?.CurrentStepNo;
-    public static string CurrentStepName => Managers.Instance?.GameFlowManager?.currentStepData?.NameInDatabase;
+    public static string CurrentStepName => Managers.Instance?.GameFlowManager?.currentStepData
+        ?.NameInDatabase;
     public static int? CurrentTurnNum => Managers.Instance?.GameFlowManager?.CurrentTurnNo;
-    public static string CurrentTurnName => Managers.Instance?.GameFlowManager?.currentTurnData?.NameInDatabase;
+    public static string CurrentTurnName => Managers.Instance?.GameFlowManager?.currentTurnData
+        ?.NameInDatabase;
 
     public static bool IsGameActive()
     {
         return CurrentStepName != null;
     }
 
-    //TODO: show the exclamation icon for custom story fragments
     //TODO: journal api to add "I signed the XYZ act" etc strings to the journal
     //TODO: require completion of custom story fragments to end the step
     public static void AddCustomBill(CustomBillData customBillData)
     {
         if (!IsGameActive())
         {
-            throw new InvalidOperationException("Cannot add a story fragment when the game is not active.");
+            throw new InvalidOperationException(
+                "Cannot add a story fragment when the game is not active.");
         }
 
         BillData billData = customBillData.ToSuzerainBillData();
 
-        Func<BillData, bool> match = d => string.Equals(d.NameInDatabase, billData.NameInDatabase, StringComparison.Ordinal);
+        // Check if the bill data already exists in the registry.
+        Func<BillData, bool> match = d => string.Equals(
+            d.NameInDatabase,
+            billData.NameInDatabase,
+            StringComparison.Ordinal);
         bool existsInRegistry = EntityDataManager.AllBillsData.Exists(match);
         if (!existsInRegistry)
         {
+            // We add the bill data to the registry on demand,
+            // since the registry resets when a game is loaded.
             EntityDataManager.AllBillsData.Add(billData);
         }
 
+        // Add it to the scene and create the token indicator (the exclamation icon).
         Managers.Instance.GameFlowManager.enabledNotDoneStoryFragments.Add(billData);
+        CreateTokenIndicator(
+            customBillData.AssignedTokenName,
+            TokenIndicatorPanel.TokenIndicatorType.StoryFragment);
+    }
 
-        //Panels.Instance.TokenIndicatorPanel.TryAddTokenIndicator(billData.AssignedTokenProperties.AssignedToken, TokenIndicatorPanel.TokenIndicatorType.StoryFragment, Panels.Instance.TokenIndicatorPanel.tokenIndicators);
-        //foreach (TokenIndicatorPanel.TokenIndicator ind in Panels.Instance.TokenIndicatorPanel.tokenIndicators)
-        //{
-        //    Melon<Core>.Logger.Msg($"Token indicator: {ind.token.tokenDataEntityName} {ind.token.tokenType} {ind.tokenIndicatorType}");
-        //    if (string.Equals(ind.token.tokenDataEntityName, billData.AssignedTokenProperties.AssignedToken, StringComparison.Ordinal))
-        //    {
-        //        Melon<Core>.Logger.Msg($"Showing token indicator for {billData.AssignedTokenProperties.AssignedToken}");
-        //        Panels.Instance.TokenIndicatorPanel.ShowTokenIndicator(ind.token);
-        //    }
-        //}
+    private static void CreateTokenIndicator(string assignedTokenName,
+        TokenIndicatorPanel.TokenIndicatorType indicatorType)
+    {
+        if (!IsGameActive())
+        {
+            throw new InvalidOperationException(
+                "Cannot add a token indicator when the game is not active.");
+        }
+
+        TokenIndicatorPanel panel = Panels.Instance.TokenIndicatorPanel;
+
+        // Check if the token indicator we're trying to create already exists.
+        Func<TokenIndicatorPanel.TokenIndicator, bool> match =
+            ind => string.Equals(
+                ind.token.tokenDataEntityName,
+                assignedTokenName,
+                StringComparison.Ordinal) && ind.tokenIndicatorType == indicatorType;
+        bool exists = panel.tokenIndicators.Exists(match);
+        if (exists)
+        {
+            return;
+        }
+
+        // TryAddTokenIndicator creates the token indicator and adds it to the list.
+        panel.TryAddTokenIndicator(
+            assignedTokenName,
+            indicatorType,
+            panel.tokenIndicators);
+
+        // Get the indicator we just created.
+        TokenIndicatorPanel.TokenIndicator newIndicator = panel.tokenIndicators.Find(match);
+        if (newIndicator == null)
+        {
+            Melon<Core>.Logger.Error(
+                $"Failed to create token indicator for token '{assignedTokenName}'.");
+            return;
+        }
+
+        // Get a template. For some reason, panel.templateTokenIndicator doesn't work,
+        // so we just grab the first one from the list of instantiated token indicators.
+        TemplateTokenIndicator indicatorTemplate = panel.instantiatedTokenIndicators[0];
+        if (indicatorTemplate == null)
+        {
+            Melon<Core>.Logger.Error(
+                $"Failed to create token indicator for token '{assignedTokenName}'. No template object found.");
+            return;
+        }
+
+        // Create the game object.
+        TemplateTokenIndicator indicatorObj = UnityEngine.Object.Instantiate(
+            indicatorTemplate,
+            indicatorTemplate.transform.parent);
+        indicatorObj.Setup(newIndicator);
+        panel.instantiatedTokenIndicators.Add(indicatorObj);
     }
 }
